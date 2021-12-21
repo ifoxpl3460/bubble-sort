cd3241da5b9d7305e5c20e4b66659ba72f6330f7
ifoxpl3460 <j.jackowski123@gmail.com>
2021-12-21 10:14:31 +0100

    better visuals

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// number of all lines
 #define n 256
 
 // sfml lib
@@ -6,22 +5,20 @@
 
 // data for lines
 struct data {
-	int height;
-	int posx;
+	int height; int posx;
 } line[n];
 
 int main () {
 
 	// variables
 	int j = 0;
-	int randomh = 0, part = 1, m = 0, r = n;
+	int randomh = 0, p = 1, m = 0, r = n;
 
 	// sfml 
-	sf::RenderWindow* win = new sf::RenderWindow (sf::VideoMode (256, 256), "bubble-sort");
+	sf::RenderWindow* win = new sf::RenderWindow (sf::VideoMode (512, 512), "bubble-sort", sf::Style::Close);
 	sf::Event* evnt = new sf::Event;
 
-	sf::RectangleShape* rectangle = new sf::RectangleShape (sf::Vector2f (4, 178));
-	rectangle->setFillColor (sf::Color (235, 149, 13));
+	sf::RectangleShape* rectangle = new sf::RectangleShape;
 
 	srand (time (NULL));
 
@@ -32,17 +29,17 @@ int main () {
 		}
 
 		// setting random height and posx for every line
-		if (part == 1) {
+		if (p == 1) {
 			for (int i = 0; i < n; i++) {
-				randomh = 1 + rand () % (256);
+				randomh = 1 + rand () % (512);
 				line[i].posx = i;
 				line[i].height = randomh;
-				if (i == n - 1) part = 2;
+				if (i == n - 1) p = 2;
 			}
 		}
 
-		// bubble sort
-		if (part == 2) {
+		// sort
+		if (p == 2) {
 			if (line[j].height < line[j + 1].height) {
 				m = line[j].height;
 				line[j].height = line[j + 1].height;
@@ -56,7 +53,7 @@ int main () {
 		for (int i = 0; i < n; i++) {
 			rectangle->setFillColor (sf::Color (0, 255, 100));
 			if (i == j) rectangle->setFillColor (sf::Color (255, 0, 0));
-			rectangle->setPosition (line[i].posx, 256);
+			rectangle->setPosition (line[i].posx + line[i - 1].posx + 1, 512);
 			rectangle->setSize (sf::Vector2f (1, line[i].height));
 			rectangle->setRotation (180);
 			win->draw (*rectangle);
@@ -74,6 +71,7 @@ int main () {
 
 	delete win;
 	delete evnt;
+	delete rectangle;
 
 	return 0;
 }
